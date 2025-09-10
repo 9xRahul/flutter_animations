96c996852e68d12c3e09c5c8585e67d1b32aeb3c
rahul <rahul.p@9xtechnology.com>
2025-09-10 11:28:03 +0530

    initial commit

---
+++ lib/models/activity_model.dart
@@ -0,0 +1,14 @@
+class Activity {
+  final String name;
+  final String location;
+  final String imageUrl;
+  final double price;
+
+  Activity({
+    required this.name,
+    required this.location,
+    required this.imageUrl,
+    required this.price,
+  });
+  
+}
